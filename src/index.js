ca81b282626d799054b89a05bf2eb5ee2a217003
Vasilisa Shokhava <vasilisa.shokhava@wolterskluwer.com>
2022-01-04 00:48:09 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    const reverse = require('type-reverse');
+    const reversed = reverse(n);
+    const reversedString = reversed.toString();
+    if (reversedString.includes('-')) {
+       const stringWithoutMinus = reversedString.replace('-', '');
+      return parseInt(stringWithoutMinus);
+    }
+    else {
+        return reversed;
+    }
 }
